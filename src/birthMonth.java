a0527cce3971dcc618e36aeb720524ea0df5c1d2
cleoc <kimuraam@mail.uc.edu>
2022-09-26 14:57:20 -0400

    Initial commit

--- src/birthMonth.java
+++ src/birthMonth.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class birthMonth {
+public class birthMonth {
+    public static void main(String[] args){
+        int birthMonth = 34;
+        System.out.println("What is your birth month? enter a integer between 1 and 12.");
+        if(birthMonth >= 1 && birthMonth <= 12)
+        {
+            System.out.println("Your birth month is: " + birthMonth);
+        }
+        else
+        {
+            System.out.println("you entered an incorrect value: " + birthMonth);
+        }
+    }
 }
