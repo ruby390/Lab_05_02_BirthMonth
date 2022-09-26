59daad90552c6803ae77bad18e956687069ef605
cleoc <kimuraam@mail.uc.edu>
2022-09-26 15:00:58 -0400

    Initial commit

--- src/birthMonth.java
+++ src/birthMonth.java
@@ -1,7 +1,7 @@
 public class birthMonth {
     public static void main(String[] args){
         int birthMonth = 34;
-        System.out.println("What is your birth month? enter a integer between 1 and 12.");
+        System.out.println("What is your birth month? enter an integer between 1 and 12.");
         if(birthMonth >= 1 && birthMonth <= 12)
         {
             System.out.println("Your birth month is: " + birthMonth);
